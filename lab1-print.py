183790f109ec2fd37374e24454f145ad2709d9db
shenyuanwu <wu220@umail.iu.edu>
2016-07-09 19:24:46 -0400

    Add files via upload

---
+++ lab1-print.py
@@ -0,0 +1,6 @@
+print('My name is Wu Shenyuan')
+print("I like to playing computer games")
+print("""Such as Dota2, starcraft and warcraft""")
+print("The \nmost I"+" "+'like for this three is'+" "+"""Dota2""")
+print('"It is "'),print("'interesting'")
+
